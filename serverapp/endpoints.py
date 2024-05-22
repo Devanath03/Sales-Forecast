f2d334d0976e9b06ebb27871b063bc32afc669c4
Devanath03 <devanathbalann@gmail.com>
2024-05-22 23:37:12 +0530

    Commit

--- serverapp/endpoints.py
+++ serverapp/endpoints.py
@@ -164,19 +164,3 @@ def project_api_routes(endpoints):
         return resp
                
     return endpoints
-
-# def create_app():
-#     web_app = Flask(__name__)  
-#     CORS(web_app)
-
-#     api_blueprint = Blueprint('api_blueprint', __name__)
-#     api_blueprint = project_api_routes(api_blueprint)
-
-#     web_app.register_blueprint(api_blueprint, url_prefix='/api')    
-
-#     return web_app
-
-# app = create_app()
-
-# if __name__ == "__main__":
-#     create_app().run(host="0.0.0.0", debug=True)
